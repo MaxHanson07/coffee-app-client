d5839de1f8dee20abce84364b1767dc82b23e3be
Dan Gentile <dangentile@ymail.com>
2020-11-11 14:06:59 -0800

    added env variable

--- src/elements/GoogleMaps/GoogleMaps.js
+++ src/elements/GoogleMaps/GoogleMaps.js
@@ -1,35 +1,72 @@
-import React from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import "./GoogleMaps.scss";
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
-import mapstyles from "./mapStyles";
+import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
+import mapStyles from "./mapStyles";
 export default function GoogleMapsElement() {
+  const [selectedState, setSelectedState] = useState(null);
+
+  useEffect(() => {
+    setSelectedState();
+  }, []);
+
+  const [markersState, setMarkersState] = useState([
+    {
+      id: 1,
+      title: "Proud Mary",
+      lat: 45.558861,
+      lng: -122.644432,
+    },
+  ]);
+
+  const mapRef = useRef();
+  const onMapLoad = useCallback((map) => {
+    mapRef.current = map;
+  }, []);
+
   const options = {
-    styles: mapstyles,
+    styles: mapStyles,
     disableDefaultUI: true,
     zoomControl: true,
   };
   const mapContainerStyle = {
-    width: "100vw",
-    height: "100vh",
+    width: "100%",
+    height: "100%",
   };
   const center = {
     lat: 45.5316,
     lng: -122.6668,
   };
+
   const { isLoaded, loadError } = useJsApiLoader({
-    googleMapsApiKey: "AIzaSyB9QBa0YuuN4CBb-4CMOs3JExZnsZ5juv0",
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
   });
   if (loadError) return "Error loading maps";
   if (!isLoaded) return "Loading maps";
 
   return (
-    <div className="GoogleMaps">
-      <GoogleMap
-        mapContainerStyle={mapContainerStyle}
-        zoom={12}
-        center={center}
-        options={options}
-      />
-    </div>
+    <>
+      <div className="GoogleMaps">
+        <GoogleMap
+          mapContainerStyle={mapContainerStyle}
+          zoom={12}
+          center={center}
+          options={options}
+          onLoad={onMapLoad}
+        >
+          {markersState.map((marker) => {
+            return (
+              <Marker
+                key={marker.id}
+                position={{ lat: marker.lat, lng: marker.lng }}
+                onClick={() => setSelectedState(marker)}
+              />
+            );
+          })}
+        </GoogleMap>
+      </div>
+      <div className="Info">
+        {!selectedState ? null : <p>{selectedState.title}</p>}
+      </div>
+    </>
   );
 }
