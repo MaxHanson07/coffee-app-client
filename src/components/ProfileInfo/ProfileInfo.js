669f1b3c9d162eef42a7baf8669be46a6f067675
Elijah B <69051669+elijah415hz@users.noreply.github.com>
2020-11-19 20:43:37 -0800

    Displaying recent cafes

--- src/components/ProfileInfo/ProfileInfo.js
+++ src/components/ProfileInfo/ProfileInfo.js
@@ -1,14 +1,44 @@
 import React from "react";
 import "./ProfileInfo.scss";
 
-export default function ProfileInfo({profileState}) {
+export default function ProfileInfo({profileState, handleCafeClick}) {
   
+  // Removes duplicate cafes
+  let check_ins = [...profileState.check_ins].reduce(function (accumulator, current) {
+    if (checkIfAlreadyExist(current)) {
+      return accumulator
+    } else {
+      return accumulator.concat([current]);
+    }
+    
+    function checkIfAlreadyExist(currentVal) {
+      return accumulator.some(function(item){
+        return (item.cafe_id.name === currentVal.cafe_id.name);
+      });
+    }
+  }, []);
+
+  if (check_ins.length > 3) {
+    check_ins = check_ins.slice(check_ins.length - 4, check_ins.length - 1)
+  }
+
   return (
     <>
       {profileState.isLoggedIn ? (
         <div className="profile-info">
         <h4>{profileState.name}</h4>
         <img className="profile-img" src={profileState.photo_url} alt="profile" />
+        <h5>Your recent cafes</h5>
+        {check_ins.map(check_in=>{
+          return (
+            <a href="#" onClick={(e)=>{
+              e.preventDefault()
+              handleCafeClick(check_in.cafe_id)
+            }}>
+            <p key={check_in.timestamp}>{check_in.cafe_id.name}</p>
+            </a>
+          )
+        })}
       </div>
         ) : null}
     </>
